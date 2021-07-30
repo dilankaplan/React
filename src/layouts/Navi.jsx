a1c1ad466d00e1c217722cb7886a455e2975fd95
dilankaplan <45143995+dilankaplan@users.noreply.github.com>
2021-07-30 20:02:01 +0300

    React

---
+++ src/layouts/Navi.jsx
@@ -0,0 +1,18 @@
+import Reader from 'react'
+import { Button, Container, Dropdown, Menu } from 'semantic-ui-react'
+import CartSummary from './CartSummary'
+
+export default function Navi() {
+    return (
+        <div>
+            <Menu inverted fixed>
+               <Container>
+                  
+                   </Container> 
+                
+            
+            </Menu>
+
+        </div>
+    );
+}
